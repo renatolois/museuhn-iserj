f6ee62c92fd7441cac1a9fd13290610d7c6621f8
Renato Lóis Marcondes da Silva <renato.l.marcondes@gmail.com>
2025-11-24 12:11:49 -0300

    commiting http_server.py

--- utilitaries/https_server.py
+++ utilitaries/https_server.py
@@ -4,13 +4,18 @@ import os
 import ssl
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 
+port = int(input())
+
 context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
-# i have used this to generate the keys: openssl req -x509 -newkey rsa:2048 -nodes -keyout /tmp/https_server_temporary_key.pem -out /tmp/https_server_temporary_cert.pem -days 365 -subj "/C=US/ST=State/L=City/O=MyOrganization/CN=localhost"
-context.load_cert_chain(certfile='/tmp/https_server_temporary_cert.pem', keyfile='/tmp/https_server_temporary_key.pem')
+# i have used this to generate the keys: openssl req -x509 -newkey rsa:2048 -nodes -keyout /tmp/https_server_temporary_key.pem -out /tmp/https_server_temporary_cert.pem -days 365 -subj "/C=US/ST=State/L=City/O=Organization/CN=localhost"
+openssl_cmd = r'openssl req -x509 -newkey rsa:2048 -nodes -keyout .https_server_temporary_key.pem -out .https_server_temporary_cert.pem -days 365 -subj "/C=US/ST=State/L=City/O=Organization/CN=localhost"'
+if not ( os.path.exists('.https_server_temporary_cert.pem') and os.path.exists('.https_server_temporary_key.pem') ):
+    os.system(openssl_cmd)
+context.load_cert_chain(certfile='./.https_server_temporary_cert.pem', keyfile='.https_server_temporary_key.pem')
 context.check_hostname = False
 
-with HTTPServer(("0.0.0.0", 4443), SimpleHTTPRequestHandler) as httpd:
+with HTTPServer(("0.0.0.0", port), SimpleHTTPRequestHandler) as httpd:
     httpd.socket = context.wrap_socket(httpd.socket, server_side=True)
-    print("Servidor HTTPS iniciado em https://localhost:4443")
+    print(f"Servidor HTTPS iniciado em https://localhost:{port}")
     httpd.serve_forever()
 
